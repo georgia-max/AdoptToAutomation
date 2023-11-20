556c0927f37c69cabe4ef33b2a79068804eacfef
georgia-max <georgialiu63@gmail.com>
2023-11-20 18:31:45 -0500

    Initial project version

---
+++ src/APM_data_preprocess.R
@@ -0,0 +1,199 @@
+source("/Users/Georgia 1/Desktop/Workload&ErrorPaper/MWL_R_Project/R/dataset_builder.R", encoding = "UTF-8")
+
+library(vtable)
+
+# Load Data
+
+#Read APM dataset.
+# df <- read_csv(
+#     "APM_class2019_Aug_Sep_Oct_2019_only_TC.csv",
+#     col_types = cols(.default = "c",
+#         DELAY_PREV_SIG_avg = col_skip(),
+#         DELAY_PREV_SIG_avg_abs_val = col_skip(), 
+#         DELAY_PREV_SIG_PASS_ONLY_avg = col_skip(), 
+#         DELAY_PREV_SIG_PASS_ONLY_avg_abs_val = col_skip(), 
+#         YEAR_OF_BIRTH = col_skip(), 
+#         FATIGUE_NEW_SHIFT_RISK = col_skip(), 
+#         GENDER = col_skip(), 
+#         FATIGUE_NEW_RISK_INDEX = col_skip(), 
+#         FATIGUE_NEW_EXPOSURE = col_skip(),
+#         DATE_EVENT = col_date(format = "%d/%m/%Y"))
+#         )
+
+# Read CRIPTON dataset.
+df_raw <- read_csv(
+    "/Users/Georgia 1/Desktop/Workload&ErrorPaper/MWL_R_Project/Data/PeterMS_Paper/CRIPTON_60_MINUTES_anonymized_all.csv",
+
+    col_types = cols(.default = "c",
+    DATE_EVENT = col_date(format = "%Y-%m-%d")
+    )
+)
+
+# # Specify the columns to transform
+# cols_to_transform <- c("MOVE_AUTO_count", "MOVE_MAN_count", "c")
+
+# Transform selected columns to numeric
+# my_data_numeric <- df %>% 
+#   mutate_at(vars(cols_to_transform), function(x) {
+#     #x <- as.numeric(x)
+#     ifelse(is.na(x), NA, x)
+#   })
+
+# Print the transformed data frame
+# print(my_data_numeric)        
+
+df <- df_raw %>%
+  mutate(
+         MOVE_ALL_count = as.numeric(gsub(",", "", MOVE_ALL_count), na.rm = TRUE),
+         MOVE_AUTO_count = as.numeric(gsub(",", "", MOVE_AUTO_count), na.rm = TRUE), # nolint
+         MOVE_MAN_count = as.numeric(MOVE_MAN_count, na.rm = TRUE),
+         ADAPTATIONS_count = as.numeric(ADAPTATIONS_count, na.rm = TRUE),
+         CHANGE_AUTO_AA_count = as.numeric(CHANGE_AUTO_AA_count, na.rm = TRUE),
+         CHANGE_AUTO_WW_count = as.numeric(CHANGE_AUTO_WW_count, na.rm = TRUE),
+         PHONE_IN_count = as.numeric(PHONE_IN_count, na.rm = TRUE),
+         PHONE_OUT_count = as.numeric(PHONE_OUT_count, na.rm = TRUE),
+         MESSAGE_OTHER_count = as.numeric(MESSAGE_OTHER_count, na.rm = TRUE),
+         MESSAGE_HUMAN_ERROR_count = as.numeric(MESSAGE_HUMAN_ERROR_count),# na.rm = TRUE),
+         JUSTIF_count = as.numeric(JUSTIF_count, na.rm = TRUE),
+         #TRAF_DENS = as.numeric(gsub(",", "", TRAF_DENS)), #na.rm = TRUE),
+         TRAF_COMP = as.numeric(gsub(",", "", TRAF_COMP)),# na.rm = TRUE),
+         #RAILWAY_DAYS = as.numeric(gsub(",", "", RAILWAY_DAYS), na.rm = TRUE),
+         #EXPERIENCE_DAYS = as.numeric(gsub(",", "", EXPERIENCE_DAYS), na.rm = TRUE),
+         #AGE = as.numeric(gsub(",", "", AGE), na.rm = TRUE)
+         )
+
+# find row indices with NAs
+# na_rows <- which(apply(is.na(df), 1, any))
+# # print rows with NAs
+# print(df[na_rows,])
+
+#Exclude Weekends and night shifts.
+df <- exclude_weekend_night(df)
+
+df <- change_column_names(df, c("MESSAGE_HUMAN_ERROR_count", "HOUR_EVENT"), 
+                          c("Error_Count", "Hour"))
+#### Manipulate the variables.
+df$Date_hour <- df$DATE_EVENT + hours(df$Hour)
+
+df$PHONE = df$PHONE_IN_count + df$PHONE_OUT_count
+# df$TotalWL <-  df$MOVE_MAN_count + df$CHANGE_AUTO_AA_count +
+#     df$CHANGE_AUTO_WW_count + df$MOVE_AUTO_count  +   df$ADAPTATIONS_count
+
+df$TotalWL <-  df$MOVE_MAN_count + df$MOVE_AUTO_count 
+
+
+df$Manual_count <- df$MOVE_MAN_count + df$CHANGE_AUTO_AA_count +
+     df$CHANGE_AUTO_WW_count + df$ADAPTATIONS_count
+
+
+
+df$Month <-  as.numeric(format(df$DATE_EVENT, format = "%m"))
+df$Year <- as.numeric(format(df$DATE_EVENT, format = "%Y"))
+df$Day <- as.numeric(format(df$DATE_EVENT, format = "%d"))
+# df$timeorder <- as.numeric(format(df$DATE_EVENT(format = "%y-%m-%d"))*100 + df$HOUR_EVENT)
+
+df$Manual_count_sq <- df$Manual_count^2
+df$MOVE_AUTO_count_sq <- df$MOVE_ALL_count^2
+
+#df[is.na(df)] <- 0
+
+
+#print dataframe class
+print(sapply(df, class))
+
+# Filter rows with NA in any column
+df <- df[complete.cases(df), ]
+print(describe(df))
+
+df$Manual_fraction <- df$MOVE_MAN_count / df$TotalWL
+df$Auto_fraction <- df$MOVE_AUTO_count / df$TotalWL
+
+df$Error_rate = df$Error_Count/ df$TotalWL
+df$Error_rate_thousand = df$Error_Count/ df$TotalWL *1000
+
+df$Manual_fraction_sq = df$Manual_fraction^2
+df$Auto_fraction_sq = df$Auto_fraction^2
+df$TotalWL_sq = df$TotalWL^2
+
+
+#Delete Rows if MOVE_ALL = 0 & ERROR != 0 #Since we view this as outliers.
+df <- df %>% filter(!(TotalWL == 0 & Error_Count != 0 ))
+
+df <- df %>% filter(!(TotalWL == 0))
+df <- df %>% filter((Error_rate <=1))
+
+#df <- df %>% filter((TotalWL <207 ))
+
+
+# Subet when Auto_Fraction = 0. Means that no one has used ADAS. 
+# df_sub <- subset(df_sub, df$Auto_fraction != 0)
+
+
+# TotalWL
+Q <- quantile(df$TotalWL, probs=c(.25, .75), na.rm = FALSE)
+iqr <- IQR(df$TotalWL)
+up <-  Q[2]+1.5*iqr # Upper Range  
+low<- Q[1]-1.5*iqr # Lower Range
+
+
+data_no_outlier<- subset(df, df$TotalWL > low & df$TotalWL < up)
+
+
+df <- data_no_outlier
+
+p <- ggplot(data = df, aes(x = "", y = TotalWL)) + 
+  geom_boxplot() #+
+#coord_cartesian(ylim = c(0, 150)) 
+ggplotly(p)
+
+
+#Correlation plot 
+########
+library("xlsx")
+df2 <- subset(df, select = c('Error_rate','Auto_fraction',"TotalWL","PHONE","MESSAGE_OTHER_count", "Hour"
+                             ,"JUSTIF_count",
+                             'ADAPTATIONS_count','TRAF_COMP'))
+
+vcorr <- round(cor(df2),3)
+
+write.xlsx(vcorr, "./Event_Based_results/Addition_manual_corr_050523.xlsx")
+
+
+
+
+
+# 
+# 
+# # Manual Count 
+# Q <- quantile(df$Manual_count, probs=c(.25, .75), na.rm = FALSE)
+# iqr <- IQR(df$Manual_count)
+# up <-  Q[2]+1.5*iqr # Upper Range  
+# low<- Q[1]-1.5*iqr # Lower Range
+# eliminated<- subset(df, df$Manual_count > (Q[1] - 1.5*iqr) & df$Manual_count < (Q[2]+1.5*iqr))
+# 
+# p <- ggplot(data = eliminated, aes(x = "", y = Manual_count)) + 
+#   geom_boxplot() #+
+#   #coord_cartesian(ylim = c(0, 150)) 
+# #ggplotly(p)
+# 
+# # Move Auto Count 
+# Q <- quantile(eliminated$MOVE_AUTO_count, probs=c(.25, .75), na.rm = FALSE)
+# iqr <- IQR(eliminated$MOVE_AUTO_count)
+# up <-  Q[2]+1.5*iqr # Upper Range  
+# low<- Q[1]-1.5*iqr # Lower Range
+# eliminated2<- subset(eliminated, eliminated$MOVE_AUTO_count > (Q[1] - 1.5*iqr) &eliminated$MOVE_AUTO_count < (Q[2]+1.5*iqr))
+# 
+# p <- ggplot(data = eliminated2, aes(x = "", y = MOVE_AUTO_count)) + 
+#   geom_boxplot() #+
+#   #coord_cartesian(ylim = c(0, 150)) 
+# #ggplotly(p)
+# 
+# nrow(eliminated2)
+# 
+# df <- eliminated2
+# # Reset index 
+# rownames(df)<- NULL
+# 
+
+
+
---
+++ src/Fraction_Model(APM).R
@@ -0,0 +1,272 @@
+library(fixest)
+library(MASS)
+source("./Plot.R")
+source("./Functions.R")
+
+library(stargazer)
+
+
+#create summary table
+# df_sum < - round(describe(df_model, fast = TRUE), 3)
+# 
+# write.csv(df_sum, file = "sumstats.csv")
+#stargazer(df_sum,title="Descriptive Satistics", type ="text",  digit =2,  out= "/APM_results/sumstats.txt")
+
+
+
+#####
+
+m1= fenegbin(Error_Count ~ TotalWL + #print dataframe class
+ +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour-1| DATE_EVENT+RAILWAY_DAYS+AGE,vcov = "twoway", data = df
+)
+m2= fenegbin(Error_Count ~ TotalWL + Manual_fraction +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour+
+               I(Manual_fraction^2)+I(TotalWL^2)| DATE_EVENT+RAILWAY_DAYS+AGE,vcov = "twoway", data = df
+)
+m3= fenegbin(Error_Count ~ TotalWL + Auto_fraction +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour-1| DATE_EVENT+RAILWAY_DAYS+AGE,vcov = "twoway", data = df
+)
+m4= fenegbin(Error_Count ~ TotalWL + Auto_fraction +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour+ 
+               I(Auto_fraction^2)+I(TotalWL^2)| DATE_EVENT+RAILWAY_DAYS+AGE,vcov = "twoway", data = df
+)
+#Why in m4, Auto_fraction is not significant but ^2 is significant 
+etable(m1,m2,m3,m4)
+
+etable(m3,m4)
+
+#normallized?
+m_nb= glm.nb(Error_Count ~ TotalWL + Auto_fraction +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour+
+            I(Auto_fraction^2)+I(TotalWL^2), data = df, start = c(0.005, 0.006, 2.1, 0.0006,-0.0001,0.004,0.0005,-5.8,-0.000005))
+m_qp= glm(Error_Count ~ TotalWL + Auto_fraction +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour+
+               I(Auto_fraction^2)+I(TotalWL^2), data = df, family = "quasipoisson")
+predicted.y = predict(m_qp, newdata=df, type="response")
+
+dnbinom(100, mu=predicted.y, size=m4$theta)
+
+est <- cbind(Estimate = coef(m4), confint(m4))
+library(VGAM)
+fit <- vglm(Error_Count ~ TotalWL + Auto_fraction +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour+
+              I(Auto_fraction^2)+I(TotalWL^2), data = df, family=negbinomial(), )
+summary(fit)
+predict(fit, se.fit=TRUE)
+
+library(ggplot2)
+
+ggplot() +
+  geom_qq(aes(sample = rstandard(fit))) +
+  geom_abline(color = "red") +
+  coord_fixed()
+
+
+
+
+
+
+
+
+
+#Test for endogenetiy of manual count 
+ob_removed<- m2$obs_selection$obsRemoved*-1
+
+df$index <- seq(1, nrow(df))
+#New dataset deleting Fixed removed. 
+df2<- df[-c(ob_removed),]
+########
+# df2$m2.res.moveman <- m2$residuals
+# 
+# m3= fenegbin(Error_Count ~ TotalWL + Manual_fraction +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour-1| DATE_EVENT+RSE_ID_ANONYM,vcov = "twoway", data = df2
+# )
+# m4= fenegbin(Error_Count ~ TotalWL + Manual_fraction +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour+
+#                csw(I(Manual_fraction^2))-1| DATE_EVENT+RSE_ID_ANONYM,vcov = "twoway", data = df2
+# )
+# etable(m3,m4)
+# 
+# Hausman_reg<- fenegbin(Error_Count ~ TotalWL + Manual_fraction  +
+#                          PHONE  + csw(I(Manual_fraction^2) +m2.res.moveman)-1
+#                        | DATE_EVENT+RSE_ID_ANONYM,vcov = "twoway", data = df2
+# )
+# summary(Hausman_reg)
+# library(lmtest)
+# HausWutest<- waldtest(Hausman_reg, .~.-m4.res.moveman)
+# print(HausWutest)
+# #H0: Y is exogeneous, H1: Y is endogenous. P = 0.02 
+# #result show that we can reject the null hypothesis because the p-value is less than 0.05.
+# #Hence, we can conclude that Yt is endogenous and the estimates of the 2SLS model are appropriate.
+# 
+# 
+# ##### Hausman Move_auto 
+# m4.1st = feols(Auto_fraction ~ TRAF_COMP+TRAF_DENS 
+#                        + PHONE + JUSTIF_count + MESSAGE_OTHER_count + Hour 
+#                        # + AGE + EXPERIENCE_DAYS + GENDER 
+#                        | DATE_EVENT+RSE_ID_ANONYM, data = df2, vcov="twoway")
+# 
+# df$m4.res.moveauto <- m4.1st$residuals
+# 
+# # linear= fenegbin(Error_Count ~ TotalWL + Auto_fraction +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour-1| DATE_EVENT+RSE_ID_ANONYM,vcov = "twoway", data = df2
+# # )
+# quadratic= fenegbin(Error_Count ~ TotalWL + Auto_fraction +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour+
+#                I(Auto_fraction^2)| DATE_EVENT+RSE_ID_ANONYM,vcov = "twoway", data = df2
+# )
+# 
+# 
+# Hausman_reg<- fenegbin(Error_Count ~ TotalWL + Auto_fraction  +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour+
+#                         I(Auto_fraction^2) +m4.res.moveauto
+#                        | DATE_EVENT+RSE_ID_ANONYM,vcov = "twoway", data = df2
+# )
+# summary(Hausman_reg)
+# etable(quadratic, Hausman_reg)
+# library(lmtest)
+# HausWutest<- waldtest(Hausman_reg, .~.-m4.res.moveauto)
+# print(HausWutest)
+# #H0: Y is Auto_fraction, H1: Y is Auto_fraction P = 0.000
+# #result show that we can reject the null hypothesis because the p-value is less than 0.05.
+# #Hence, we can conclude that Yt is endogenous and the estimates of the 2SLS model are appropriate.
+# 
+# 
+
+#########
+#Hausaman Test function is in Functions.R script
+#Testing Auto_fraction Endogenous 
+#1ststage 
+formula1 <- Auto_fraction ~ TRAF_COMP+TRAF_DENS + PHONE + JUSTIF_count + MESSAGE_OTHER_count + Hour |DATE_EVENT+RAILWAY_DAYS+AGE
+#WithoutResiduals 
+formula2 <- Error_Count ~ TotalWL + Auto_fraction +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour+
+  I(Auto_fraction^2)+I(TotalWL^2)|DATE_EVENT+RAILWAY_DAYS+AGE
+#With Residuals 
+formula3<- Error_Count ~ TotalWL + Auto_fraction +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour+
+  I(Auto_fraction^2)+I(TotalWL^2)+residuals|DATE_EVENT+RAILWAY_DAYS+AGE
+
+
+Reg_2nd<- Hasusman_Test(formula1, formula2,formula3, df2)
+summary(Reg_2nd)
+#Result. Auto_fraction is slightly endogenous  
+
+#Testing Manual_fraction Endogenous 
+#1ststage 
+formula1 <- Manual_fraction ~ TRAF_COMP+TRAF_DENS + PHONE + JUSTIF_count + MESSAGE_OTHER_count + Hour |DATE_EVENT+RAILWAY_DAYS+AGE
+#WithoutResiduals 
+formula2 <- Error_Count ~ TotalWL + Manual_fraction +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour+
+  I(Manual_fraction^2)+I(TotalWL^2)|DATE_EVENT+RAILWAY_DAYS+AGE
+#With Residuals 
+formula3<- Error_Count ~ TotalWL + Manual_fraction +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour+
+  I(Manual_fraction^2)+I(TotalWL^2)+residuals|DATE_EVENT+RAILWAY_DAYS+AGE
+
+
+Reg_2nd<- Hasusman_Test(formula1, formula2,formula3, df2)
+summary(Reg_2nd)
+#Result. Manual_fraction is endogenous  
+
+#Testing TWL Endogenous 
+#1ststage 
+formula1 <- TotalWL ~ TRAF_COMP+TRAF_DENS + PHONE + JUSTIF_count + MESSAGE_OTHER_count + Hour |DATE_EVENT+RAILWAY_DAYS+AGE
+#WithoutResiduals 
+formula2 <- Error_Count ~ TotalWL + Auto_fraction +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour+
+  I(Auto_fraction^2)+I(TotalWL^2)|DATE_EVENT+RAILWAY_DAYS+AGE
+#With Residuals 
+formula3<- Error_Count ~ TotalWL + Auto_fraction +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour+
+  I(Auto_fraction^2)+I(TotalWL^2)+residuals|DATE_EVENT+RAILWAY_DAYS+AGE
+
+Reg_2nd<- Hasusman_Test(formula1, formula2,formula3, df2)
+summary(Reg_2nd)
+#Result. TWL is exogeous 
+
+#####
+########
+library(MASS)
+
+# nbin= fepois(Error_Count ~ TotalWL + Auto_fraction +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour+
+#                I(Auto_fraction^2)+I(TotalWL^2)| DATE_EVENT+RAILWAY_DAYS+AGE,vcov = "twoway", data = df
+# )
+
+m4= glm(Error_Count ~ TotalWL + Auto_fraction +PHONE+JUSTIF_count +# MESSAGE_OTHER_count + Hour+
+               I(Auto_fraction^2)+I(TotalWL^2), family = negative.binomial(0.5), data = df
+)
+
+predict.data$predicted_all <- predict(m4, predict.data, type = 'response')
+predict.data$predicted_all_link <- predict(m4, predict.data, type = 'link')
+
+
+m4=  glm.nb(Error_Count ~ TotalWL + Auto_fraction +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour+
+          I(Auto_fraction^2)+I(TotalWL^2), data = df)
+#m2_nb= glm.nb(Error_Count ~ TotalWL + Auto_fraction +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour+DATE_EVENT+I(Auto_fraction^2)+I(TotalWL^2),data = df)
+
+#m2_nb= glm.nb(Error_Count ~ TotalWL + Auto_fraction +PHONE+ JUSTIF_count + MESSAGE_OTHER_count + Hour+EXPERIENCE_DAYS+DATE_EVENT+
+#               I(Auto_fraction^2)+I(TotalWL^2),data = df)
+predict.data <- setNames(data.frame(matrix(0, ncol = 9, nrow =100)), c('TotalWL','Auto_fraction', 'PHONE','JUSTIF_count' ,
+                                                                       'MESSAGE_OTHER_count','Hour','RAILWAY_DAYS','AGE','DATE_EVENT'))
+# predict.data$Auto_fraction<-predict.data$Auto_fraction*0.01
+#predict.data$DATE_EVENT <- as.character(predict.data$DATE_EVENT)
+predict.data1<- predict_X(Auto_fraction, m_qp, predict.data)
+
+dev.off()
+
+plt<- plotCI(predict.data1, m_qp, Auto_fraction)
+
+ggsave("./APM_results/Auto_fraction.png", width = 6, height = 4, dpi = 300, plot = plt)
+
+#effect_plot(m_qp, pred = Auto_fraction, interval = TRUE, rug = TRUE)
+
+#ggplot(predict.data, aes(x=Auto_fraction, y=predicted_all)) + geom_point()
+
+
+######
+
+predict.data <- setNames(data.frame(matrix(0, ncol = 11, nrow =2000)), c('TotalWL','Auto_fraction', 'JUSTIF_count','PHONE' , 'TRAF_COMP'
+                                                                       ,'MESSAGE_OTHER_count','DATE_EVENT','RSE_ID_ANONYM','Hour'))
+#predict.data$DATE_EVENT <- as.character(predict.data$DATE_EVENT)
+#predict.data$RSE_ID_ANONYM <- as.character(predict.data$RSE_ID_ANONYM)
+predict.data<- predict_X(TotalWL, m_qp, predict.data )
+dev.off()
+#source("Plot.R", print.eval=TRUE)
+#png(filename="MVE_MAN.png")
+
+plt<- plotCI(predict.data, m_qp, TotalWL,"TotalWorkload")
+ggsave("./APM_results/TotalWL.png", width = 6, height = 4, dpi = 300, plot = plt)
+
+summary(m2_nb)
+
+#Joint Hypothesis to test if Auto_fraction is worth putting here 
+#######
+
+restricted= fenegbin(Error_Count ~ TotalWL  +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour
+             +I(TotalWL^2)| DATE_EVENT+RSE_ID_ANONYM,vcov = "twoway", data = df
+)
+
+anova(m4, restricted)
+HausWutest<- waldtest(m4, .~.-Auto_fraction.-I(Auto_fraction^2))
+print(HausWutest)
+#Not sure if this is done properly, but the idea is to test that joint hypthesis to see if Auto_fraction =0 is rejected. 
+#If so, we do not incluede this vars in the model. 
+#https://www.econometrics-with-r.org/7-3-joint-hypothesis-testing-using-the-f-statistic.html
+#https://www.statalist.org/forums/forum/general-stata-discussion/general/1408413-significance-level-of-quadratic-term
+
+library(modelsummary)
+library(gt)
+library(dplyr)
+
+Error_Count ~ TotalWL + Auto_fraction +PHONE+JUSTIF_count + MESSAGE_OTHER_count + Hour+
+  I(Auto_fraction^2)+I(TotalWL^2)| DATE_EVENT+RAILWAY_DAYS+AGE
+
+cm <- c( 'TotalWL' = 'Total WL', 'Auto_fraction' = 'Automaton Percentage',
+         'I(Auto_fraction^2)' = 'Auto_fraction^2',
+         'I(TotalWL^2)' = 'TotalWL^2', 'Manual_fraction' = "Manual Usage"
+         # ,'MOVE_MAN_resi' ='Manual Residual','MOVE_AUTO_resi' ='Monitoring Residual',
+         #'I(MOVE_MAN_resi^2)' = 'MOVE_MAN_resi_Sq','I(MOVE_AUTO_resi^2)' = 'MOVE_AUTO_resi_Sq'
+)
+
+cap <- 'Other Models'
+#omit<- 'RMSE|BIC'
+estimate <- "{estimate} ({std.error}){stars}"
+#gof <-  c("nobs", "AIC","logLik")
+#gof_map <- c("AIC", "loglik")
+note <- list('Signif. Codes: 0 “***” 0.001 “**” 0.01 “*” 0.05 “.” 0 “ “ 1 ')
+models<- list("(1) Linear"=m3,"(2) Quadratic "=m4)
+
+tab<- modelsummary(models, stars = TRUE,  output = "gt", estimate  = estimate,#gof_map =gof,
+                   #coef_map = cm,
+                   statistic = NULL, coef_omit = "theta",
+                   title=cap, notes =note
+                   # , gof_omit = omit
+)
+
+#library("webshot2")
+gt::gtsave(tab,filename = "Auto_Manual_table2.docx")
+
